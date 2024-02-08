329d6bbff29136e9830385c284fd042ec25b57b6
rambus2006 <bangminseo50@gmail.com>
2024-02-08 11:37:51 +0900

    삭제 기능 구현

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,17 +1,11 @@
-import React from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
-const TodoList = ({ todos, onRemove, onToggle }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          key={todo.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
       ))}
     </div>
   );
