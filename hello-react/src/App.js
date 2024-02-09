223ee006b35b0d0efd710bcc72f5fe8a2a70d317
rambus2006 <bangminseo50@gmail.com>
2024-02-09 18:54:49 +0900

    2.4.5 undefined를 렌더링 하지 않기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -21,9 +21,21 @@ import "./App.css";
 //   const name = "뤼액트";
 //   return <div>{name === "리액트" && <h1>리액트입니다.</h1>}</div>;
 // }
-function App() {
-  const name = "뤼액트";
-  return <div>{name === "리액트" && <h1>리액트입니다.</h1>}</div>;
-}
 
+//2.4.5 undefind를 렌더링 하지 않기
+//1. OR 연산자를 이용하여 오류 방지
+// function App() {
+//   const name = undefined;
+//   return name || "값이 undefined 입니다.";
+// }
+//2. JSX 내부에서 undefined를 렌더링하는 것은 ㄱㅊ
+// function App() {
+//   const name = undefined;
+//   return <div>{name}</div>;
+// }
+//3. name 값이 undefined 일 때 보여주고 싶은 문구가 있다면 다음과 같이 코드를 작성하면 된다.
+function App() {
+  const name = undefined;
+  return <div>{name || "리액트"}</div>;
+}
 export default App;
