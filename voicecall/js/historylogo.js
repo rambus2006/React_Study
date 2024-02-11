3982605039e8efa87dd17e37fbaf7069e20c4f1c
rambus2006 <bangminseo50@gmail.com>
2024-02-11 21:40:07 +0900

    voicecall 성우의 역사 홈화면구현

---
+++ voicecall/js/historylogo.js
@@ -0,0 +1,40 @@
+// const tooniverse = document.getElementsByClassName("img");
+
+//1. 이미지 x,y값 구하기
+//2. 이미지의 가운데값 구하기
+//---반복,
+//3. 45도로 움직이기
+//4.벽에 ㅔ닿으면 튕기기
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
