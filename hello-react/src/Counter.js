d729f86c62b0490ce1b7bf6d28f7dfb2ee86510d
rambus2006 <bangminseo50@gmail.com>
2024-02-13 20:21:58 +0900

    state를 constructor에서 꺼내기, this.setState 객체 대신 함수인자 전달하기

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -21,9 +21,20 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           //onclick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다.
-          onClick={() => {
+            onClick={() => {
             //this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
-            this.setState({ number: number + 1 });
+            // this.setState({ number: number + 1 });
+            // this.setState({ number: this.state.number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            //위 코드와 아래 코드는 완전히 똑같은 기능을 한다.
+            //아래 코드는 함수에서 바로 객체를 반환한다는 의미이다.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1
