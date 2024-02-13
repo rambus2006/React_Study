fb9109f5cdddf14681668c58067e655010e86e00
rambus2006 <bangminseo50@gmail.com>
2024-02-13 19:25:55 +0900

    클래슿ㅇ 컴포넌트로 변환

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,16 +1,30 @@
 import PropTypes from "prop-types";
+import { Component } from "react";
+// 함수형컴포넌트
+// const MyComponent = ({ name, favoriteNumber, children }) => {
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {name}입니다.
+//       <br />
+//       children 값은 {children}입니다.
+//       <br />
+//       제가 좋아하는 숫자는 {favoriteNumber}입니다.
+//     </div>
+//   );
+// };
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name}입니다.
-      <br />
-      children 값은 {children}입니다.
-      <br />
-      제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>
-  );
-};
+class MyComponent extends Component {
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name} 입니다. children 값은 {children} 입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+      </div>
+    );
+  }
+}
 // props의 기본값 설정
 MyComponent.defaultProps = {
   name: "기본이름",
