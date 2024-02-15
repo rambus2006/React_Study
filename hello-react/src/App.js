09b0eec57c56242a7fe76f09824e03f1aea51193
rambus2006 <bangminseo50@gmail.com>
2024-02-16 00:15:30 +0900

    15일 내용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -130,18 +130,30 @@
 // import ScrollBox from "./ScrollBox";
 
 import { Component } from "react";
-import IterationSample from "./IterationSample";
+// import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
   // return <MyComponent name='react'/>; 컴포넌트를 사용할 때 props값 지정하기
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
-      // <div>
-      //   <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-      //   <button onClick={() => this.scrollBox.scrollToBottom()}>
-      //     맨 밑으로
-      //   </button>
-      // </div>
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
     );
   }
 }
