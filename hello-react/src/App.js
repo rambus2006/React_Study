0181e29d0b179875ce35559ec069fc4bc7b38662
rambus2006 <bangminseo50@gmail.com>
2024-02-16 22:26:01 +0900

    useEffect

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -129,32 +129,38 @@
 // import ValidationSample from "./ValidationSample";
 // import ScrollBox from "./ScrollBox";
 
-import { Component } from "react";
-// import IterationSample from "./IterationSample";
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  // return <MyComponent name='react'/>; 컴포넌트를 사용할 때 props값 지정하기
-  state = {
-    color: "#000000",
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+// import { Component } from "react";
+// // import IterationSample from "./IterationSample";
+// import LifeCycleSample from "./LifeCycleSample";
+// import ErrorBoundary from "./ErrorBoundary";
+// function getRandomColor() {
+//   return "#" + Math.floor(Math.random() * 16777215).toString(16);
+// }
+// class App extends Component {
+//   // return <MyComponent name='react'/>; 컴포넌트를 사용할 때 props값 지정하기
+//   state = {
+//     color: "#000000",
+//   };
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor(),
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>랜덤색상</button>
+//         <ErrorBoundary>
+//           <LifeCycleSample color={this.state.color} />
+//         </ErrorBoundary>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+// import Counter from "./Counter2";
+import Info from "./info";
+const App = () => {
+  return <Info />;
+};
 export default App;
