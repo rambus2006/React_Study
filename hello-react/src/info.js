51c95dc49012dfc5518e23510dcc8f1a8214902a
rambus2006 <bangminseo50@gmail.com>
2024-02-16 22:29:22 +0900

    특정값이 업데이트 될 때만 실행하고 싶을 때

--- hello-react/src/info.js
+++ hello-react/src/info.js
@@ -5,12 +5,12 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   useEffect(() => {
-    console.log("마운트될 때만 실행됩니다.");
+    console.log(name);
     // console.log({
     //   name,
     //   nickname,
     // });
-  }, []);
+  }, [name]);
   const onChangeName = (e) => {
     setName(e.target.value);
   };
