59f1ad5f00979435eb7215ee204d152bb6efb452
rambus2006 <bangminseo50@gmail.com>
2024-02-21 18:20:01 +0900

    useMemo

--- Hooks-study/hooks-tutorial/src/App.js
+++ Hooks-study/hooks-tutorial/src/App.js
@@ -1,22 +1,25 @@
 // import Counter from "./Counter";
-import Info from "./info.js";
-import { useState } from "react";
+// import Info from "./info.js";
+// import { useState } from "react";
 import "./App.css";
-
+// import Counter from "./Counter";
+// import Info from "./info";
+import Average from "./Average";
 const App = () => {
-  const [visable, setVisable] = useState(false);
+  // const [visable, setVisable] = useState(false);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setVisable(!visable);
-        }}
-      >
-        {visable ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visable && <Info />}
-    </div>
+    // <div>
+    //   <button
+    //     onClick={() => {
+    //       setVisable(!visable);
+    //     }}
+    //   >
+    //     {visable ? "숨기기" : "보이기"}
+    //   </button>
+    //   <hr />
+    //   {visable && <Info />}
+    // </div>
+    <Average />
   );
 };
 
--- Hooks-study/hooks-tutorial/src/info.js
+++ Hooks-study/hooks-tutorial/src/info.js
@@ -1,40 +1,55 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useReducer } from "react";
+// const Info = () => {
+//   const [name, setName] = useState("");
+//   const [nickname, setNickname] = useState("");
 
+//   useEffect(() => {
+// console.log("렌더링이 완료되었습니다.");
+// console.log({
+//   name,
+//   nickname,
+// });
+// console.log("마운트될 때만 실행됩니다.");
+// console.log(name);
+// console.log("effect");
+// console.log(name);
+// return () => {
+//   console.log("cleanup");
+//   console.log(name);
+// };
+//   return () => {
+//     console.log("unmount");
+//   };
+// }, []);
+
+// const onChangeName = (e) => {
+//   setName(e.target.value);
+// };
+// const onChangeNickname = (e) => {
+//   setNickname(e.target.value);
+// };
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
 
-  useEffect(() => {
-    // console.log("렌더링이 완료되었습니다.");
-    // console.log({
-    //   name,
-    //   nickname,
-    // });
-    // console.log("마운트될 때만 실행됩니다.");
-    // console.log(name);
-    console.log("effect");
-    // console.log(name);
-    // return () => {
-    //   console.log("cleanup");
-    //   console.log(name);
-    // };
-    return () => {
-      console.log("unmount");
-    };
-  }, []);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
-
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
@@ -49,4 +64,5 @@ const Info = () => {
     </div>
   );
 };
+
 export default Info;
